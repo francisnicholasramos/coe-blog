b549271ab20772a054e54e2179c283679d0647d0
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2026-02-12 02:25:39 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+    return (
+        <div className="bg-blue-500 text-white p-4 rounded-2xl">
+            <h1 className="text-2xl font-bold">Tailwind v4 is working!</h1>
+        </div>
+    )
+}
+
+export default App
