563d2f009a3f0c92a723b579a50bcbd59b3a441b
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2026-02-19 20:59:13 +0800

    add specific endpoint for fetching blog post by id

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Posts from './components/posts/Posts'
+import PostDetail from './components/posts/components/PostDetail'
 import RouteSwitch from './routes/RouteSwitch'
 import { Header } from './components/Header'
+import { Footer } from './components/Footer'
 
 const App = () => {
     return (
-        <div>
+        <div className='min-h-screen flex flex-col'>
             <RouteSwitch 
                 Header={<Header />}
                 pages={[
@@ -13,9 +15,16 @@ const App = () => {
                             <Posts />
                         ),
                         path: "/"
+                    },
+                    {
+                        element: (
+                            <PostDetail />
+                        ), 
+                        path: "/:username/:postId"
                     }
                 ]}
             />
+            <Footer />
         </div>
     )
 }
