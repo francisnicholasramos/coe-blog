740959431f48fb6fd93fccfe6ea7a67b47472ba5
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2026-03-03 07:44:59 +0800

    add refreshToken method that will renew expired accessTokens

---
+++ src/utils/api.ts
@@ -0,0 +1,45 @@
+const API_URL = `${import.meta.env.VITE_API_URL}`;
+
+async function refreshToken(): Promise<boolean> {
+    try {
+        const res = await fetch(`${API_URL}/refresh-token`, {
+            method: 'POST',
+            credentials: 'include'
+        })
+        return res.ok;
+    } catch {
+        return false;
+    }
+
+}
+
+export async function apiFetch(
+    endpoint: string,
+    options: RequestInit = {}
+): Promise<Response> {
+    const config: RequestInit = {
+        ...options,
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+            ...options.headers
+        },
+    };
+
+    let res = await fetch(`${API_URL}${endpoint}`, config);
+
+    if (res.status === 401) {
+        const refreshed = await refreshToken();
+
+        if (refreshed) {
+            await new Promise(r => setTimeout(r, 100)); // wait 100ms for cookie to update
+            res = await fetch(`${API_URL}${endpoint}`, config);
+        } else {
+            window.location.href = '/login';
+        }
+    }
+
+    return res;
+}
+
+
